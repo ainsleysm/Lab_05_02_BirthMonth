81583f349ad20afb9d7a15a035d86da5477f66c7
ainsleysm <smithainsley21@gmail.com>
2022-09-22 16:48:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int month = 15;
+
+        if(month >= 1 && month <= 12){
+            System.out.println("Your birth month is: " + month);
+        } else {
+           System.out.println("You entered an incorrect month value: " + month);
+        }
+    }
+}
